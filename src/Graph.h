53a4ca3e5df0342808c3862406bd79cd07a54ac0
zetathealmighty <ti.gouveia21@gmail.com>
2023-04-03 23:26:06 +0100

    fixed the previous merge

--- src/Graph.h
+++ src/Graph.h
@@ -16,7 +16,21 @@ public:
     bool addStation(Station station);
     bool addNetwork(std::string origStationName, std::string destStationName, int capacity, Service service);
     std::unordered_map<std::string, Station *> getStationSet() const;
+
+    //Basic Service Metrics
+    double maxFlowPair(Station *s, Station *t);
+    std::vector<Station *> getPairsWithMaxFlow();
+    //TODO - 2.3. Municipios/Distritos com maior flow?
+    double maxStationFlow(Station *station);
+
+    //Operation Cost Optmization
+    double costOptmizationMaxFlowPair(Station *s, Station *t);
+
+    //Reliability and Sensitivity to Line Failures
+    Graph generateSubGraph();
+    std::vector<std::pair<Station,int>> mostAffectedStations(const Graph& subgraph, int k);
 };
 
 
 #endif //PROJECT1_GRAPH_H
+
